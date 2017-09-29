144b700362f612de45f60354175d4b5eeca3254b
X4Ker <32191102+X4Ker@users.noreply.github.com>
2017-09-29 12:21:23 +0300

    Update max_in_array0.0.3.cpp

--- max_in_array0.0.3.cpp
+++ max_in_array0.0.3.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 int main() {
 	int a[10], b[10], l;
-	int max = 0;
+	int max = -2147483648;
 	for (int i = 0; i < 10; i++) {
 		if (!(cin >> a[i])) {
 			cout << "An error has occurred while reading numbers";
